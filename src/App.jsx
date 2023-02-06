90f60b02b7001f9801754591cc29ed1ef46d3b52
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-02-06 20:54:08 -0300

    Iniciando Projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css'
+import PoweredImage from './assets/powered.png'
 
 function App() {
 
   return (
-   <>
-   
-   </>
+  <div className={styles.main}>
+    <header>
+      <div className={styles.headerContainer}>
+        <img src={PoweredImage} alt="Logo" width={150}/>
+      </div>
+    </header>
+  </div>
   )
 }
 
