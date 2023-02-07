db70dcd21f084b5664a26a2c114de7d1beca197d
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-02-06 21:36:53 -0300

    Left and Right areas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import styles from './App.module.css'
 import PoweredImage from './assets/powered.png'
 
 function App() {
-
+  const [heightField, setHeightField] = useState(0);
+  const [weightField, setWeightField] = useState(0);
+  
   return (
   <div className={styles.main}>
     <header>
@@ -14,7 +16,16 @@ function App() {
 
     <div className={styles.container}>
       <div className={styles.leftSide}>
-          ...
+          <h1>Calcule seu IMC</h1>
+          <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+
+          <input 
+          type="number"
+          placeholder="Digite a sua altura: "
+          value={heightField > 0 ? heightField : ""} //Deixar o placeholder até que haja algum valor
+          onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+          
       </div>
       <div className={styles.rightSide}>
           ...
